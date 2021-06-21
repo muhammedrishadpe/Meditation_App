593368ee6e218b6bd204d471ff5735fbf745d7f9
muhammedrishadpe <rishadpe@gmail.com>
2021-06-21 11:41:05 +0530

    Added JS

---
+++ app.js
@@ -0,0 +1,25 @@
+const app = () => {
+    const song = document.querySelector('.song');
+    const play = document.querySelector('play');
+    const outline = document.querySelector('.moving-outline circle');
+    const video = document.querySelector('.vid-container video');
+
+    //sounds
+    const sounds = document.querySelectorAll('.sound-picker button');   
+    //Time Display
+    const timeDisplay = document.querySelector('.time-display');
+    //Get the length of the outline
+    const outlineLength = outline.getTotalLength();
+    //Duration
+    let fakeDuration = 600;
+
+    outline.style.strokeDasharray = outlineLength;
+    outline.style.strokeDashoffset = outlineLength;
+
+    //play sound
+       play.addEventListener('click', () => {
+        song.play();
+       });
+};
+
+app();
